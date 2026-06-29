393ff0a4e90c3feabc560fe9a599e9c528866691
Sean Murphy <sean@iamseanmurphy.com>
2026-06-29 13:47:08 +0000

    7.2.1

--- woocommerce-delivery-notes/includes/class-woocommerce-delivery-notes.php
+++ woocommerce-delivery-notes/includes/class-woocommerce-delivery-notes.php
@@ -26,7 +26,7 @@ final class WooCommerce_Delivery_Notes {
 	 *
 	 * @var string
 	 */
-	protected static $plugin_version = '7.2.0';
+	protected static $plugin_version = '7.2.1';
 
 	/**
 	 * Minimum version of WordPress required.
@@ -250,10 +250,10 @@ final class WooCommerce_Delivery_Notes {
 			return true;
 		}
 
-		self::include_file( 'class-wcdn-notices.php' );
+		self::include_file( 'admin/class-notices.php' );
 
 		foreach ( $messages as $index => $message ) {
-			WCDN_Notices::add_notice(
+			Notices::add_notice(
 				'plugin_installation_error_notice_' . $index,
 				$message
 			);
--- woocommerce-delivery-notes/vendor/composer/installed.php
+++ woocommerce-delivery-notes/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'd4557c2abf25ef75d332e81db3c5f4a95e124cf0',
+        'reference' => '3a076144b69c4d430789b3d83c8c033c64743386',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'd4557c2abf25ef75d332e81db3c5f4a95e124cf0',
+            'reference' => '3a076144b69c4d430789b3d83c8c033c64743386',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
